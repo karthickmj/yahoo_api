0f255b9667fa9ef4d58ff824cc26a9e7be443d84
Mattéo Bernard <114112310+SuperWD40@users.noreply.github.com>
2023-01-19 18:10:06 +0100

    Update __init__.py

--- yahoo_api/__init__.py
+++ yahoo_api/__init__.py
@@ -1,5 +1,6 @@
 from .info import get_info
 from .history import get_history
+from .history import get_dividend
 from .finance import get_financials
 
 class ticker:
@@ -8,7 +9,10 @@ class ticker:
     
     def history(self, period, continuous = False):
         return get_history(self.ticker, period, continuous)
-    
+
+    def dividend(self, period):
+        return get_dividend(self.ticker, period)
+
     def info(self):
         return get_info(self.ticker)
     
