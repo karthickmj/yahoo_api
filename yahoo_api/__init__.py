7fa376c2c239579ef905cb6a5246a2a73c0ff5e9
Mattéo Bernard <114112310+SuperWD40@users.noreply.github.com>
2023-02-02 07:39:04 +0100

    Update __init__.py
    New method: 'statistics', 'holders', 'profile',
    changing 'profile' into 'summary' to clearer understanding of the library

--- yahoo_api/__init__.py
+++ yahoo_api/__init__.py
@@ -1,7 +1,9 @@
-from .info import get_info
-from .history import get_history
-from .history import get_dividend
+from .summary import get_summary
+from .history import get_history, get_dividend
 from .finance import get_financials
+from .profile import get_profile
+from .holders import get_holders
+from .statistics import get_statistics
 
 class ticker:
     def __init__(self, ticker):
@@ -13,8 +15,17 @@ class ticker:
     def dividend(self, period):
         return get_dividend(self.ticker, period)
 
-    def info(self):
-        return get_info(self.ticker)
+    def summary(self):
+        return get_summary(self.ticker)
     
-    def financial(self, statement):
+    def financials(self, statement):
         return get_financials(self.ticker, statement)
+
+    def profile(self):
+        return get_profile(self.ticker)
+
+    def holders(self):
+            return get_holders(self.ticker)
+
+    def statistics(self):
+            return get_statistics(self.ticker)
